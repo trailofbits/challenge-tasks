0af1a44c8fa00fd1f00d3adf13aba510da4aa972
Kelly Kaoudis <kelly.kaoudis@trailofbits.com>
2024-12-17 13:57:36 -0700

    sparsely checks out this repo to obtain the challenges

--- startup.sh
+++ startup.sh
@@ -4,7 +4,7 @@
 set -eux
 
 sudo apt-get -y update
-sudo apt-get -y install build-essential libini-config-dev libseccomp-dev        make python3-pip python3-virtualenv radare2 qemu-system
+sudo apt-get -y install build-essential libini-config-dev libseccomp-dev make protobuf-compiler python3-pip python3-virtualenv qemu-system radare2
 
 echo "Setting up Python..."
 virtualenv -p /usr/bin/python3 venv
@@ -16,6 +16,7 @@ sudo chown -Rv _apt:root /var/cache/apt/archives/partial/
 sudo chmod -Rv 700 /var/cache/apt/archives/partial/
 wget https://github.com/pwndbg/pwndbg/releases/download/2024.08.29/pwndbg_2024.08.29_amd64.deb
 sudo apt install ./pwndbg_2024.08.29_amd64.deb
+rm pwndbg_2024.08.29_amd64.deb
 
 echo "Setting up GEF..."
 bash -c "$(wget https://gef.blah.cat/sh -O -)"
@@ -27,13 +28,12 @@ if [ ! -d "preeny" ] ; then
     make CFLAGS=-w
 fi
 
-echo "Cleaning up the working environment..."
-cd ~
-rm pwndbg_2024.08.29_amd64.deb
-
-echo "Setting up the challenges..."
-git clone https://github.com/trailofbits/challenge-tasks.git
+echo "Getting the challenges..."
+git clone --no-checkout https://github.com/trailofbits/challenge-tasks.git
 cd challenge-tasks
+git sparse-checkout init
+git sparse-checkout set challenges README.md
+git checkout main
 mv challenges ~/challenges
 mv README.md ~/README.md
 cd ~
