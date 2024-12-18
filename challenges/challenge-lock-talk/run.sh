06053aa1b0d0db9bb83b161e15fa845d35c80244
Kelly Kaoudis <kelly.kaoudis@trailofbits.com>
2024-12-18 14:39:50 -0700

    check if image exists before spending time and space rebuilding it

--- challenges/challenge-lock-talk/run.sh
+++ challenges/challenge-lock-talk/run.sh
@@ -13,6 +13,8 @@ if [ ! -d challenge ]; then
     rm -rf cybench
 fi
 
-docker build -t locktalk .
+if [ $(docker image inspect locktalk:latest >/dev/null 2>&1; echo $?) -ne 0 ]; then
+    docker build -t locktalk .
+fi
 docker run -it -d -p 1337:1337 --rm --name=locktalk locktalk
 source ~/venv/bin/activate
--- challenges/challenge-quick-scan/run.sh
+++ challenges/challenge-quick-scan/run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-docker build -t quickscan .
+if [ $(docker image inspect quickscan:latest >/dev/null 2>&1; echo $?) -ne 0 ]; then
+    docker build -t quickscan .
+fi
 docker run -it -d -p 1337:1337 --rm --name=quickscan quickscan
 source ~/venv/bin/activate
